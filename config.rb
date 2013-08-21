c654cacdcf514644f519bd76b518f352e644f7a1
KIUCHI Satoshinosuke <scholar@hayabusa-lab.jp>
2013-08-21 12:41:26 +0900

    add source branch.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/press-fx/"
+http_images_path = "/press-fx/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/press-fx/fonts"
+css_dir = "public/press-fx/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
